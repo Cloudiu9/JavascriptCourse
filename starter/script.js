494544caccf365500ad74cec7f85442fed9c207d
Cloudiu9 <claudiubordea5@gmail.com>
2025-07-18 14:05:05 +0300

    Creating classes to manage workout data

--- starter/script.js
+++ starter/script.js
@@ -11,6 +11,58 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+// 07. Managing Workout data with Classes
+class Workout {
+  // very new/recent class fields in JS
+  date = new Date();
+
+  // any object should have some kind of unique identifier so we can later identify it with that ID IMP
+  id = (Date.now() + '').slice(-10); // usually we have a library in charge of class id's, here we're just converting the date to a string and taking the last 10 numbers IMP
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+
+    this.calcPace();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// testing if classes work
+const run1 = new Running([39, -12], 5, 2, 24, 178);
+const cycling1 = new Cycling([39, -12], 27, 95, 523);
+console.log(run1, cycling1);
+
+//////////////////////////////////////////
+// APPLICATION ARCHITECTURE
 // 05. Refactoring the code with classes
 class App {
   #map;
