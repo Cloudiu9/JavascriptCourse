46cc84818d319dfb6307b4c25b1578f58ac54d81
Cloudiu9 <claudiubordea5@gmail.com>
2025-08-14 22:07:40 +0300

    Convert 16-Asynchronous from submodule to normal folder

---
+++ 16-Asynchronous/starter/script.js
@@ -0,0 +1,129 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+
+// https://countries-api-836d.onrender.com/countries/
+
+// 01. Simple AJAX calls IMP
+
+// Old school XML Http Request
+/*
+const getCountryDataAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+
+  request.open(
+    'GET',
+    `https://countries-api-836d.onrender.com/countries/name/${country}`
+  );
+
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.response);
+    // Render counry 1
+    renderCountry(data);
+
+    // Get neighobour country(2)
+    const neighbour = data.borders?.[0];
+
+    if (!neighbour) return;
+
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+
+    request2.open(
+      'GET',
+      `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
+    );
+
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+getCountryDataAndNeighbour('portugal');
+*/
+
+// const request = new XMLHttpRequest();
+
+// request.open(
+//   'GET',
+//   `https://countries-api-836d.onrender.com/countries/name/${country}`
+// );
+
+// request.send();
+
+// MODERN WAY OF AJAX CALLING(fetch API) IMP
+
+// returns a "Promise" IMP
+// object used as a placeholder for the future result of an asynchronous operation
+// OR a container for a future value
+
+// used to 'chain promises' ==> escaping callback hell IMP
+// const request = fetch(
+//   'https://countries-api-836d.onrender.com/countries/name/portugal'
+// );
+
+const renderCountry = function (data, className = '') {
+  const html = `
+            <article class="country ${className}">
+            <img class="country__img" src="${data.flag}" />
+            <div class="country__data">
+              <h3 class="country__name">${data.name}</h3>
+              <h4 class="country__region">${data.region}</h4>
+              <p class="country__row"><span>👫</span>${(
+                data.population / 1000000
+              ).toFixed(1)} Million</p>
+              <p class="country__row"><span>🗣️</span>${
+                data.languages[0].name
+              }</p>
+              <p class="country__row"><span>💰</span>${
+                data.currencies[0].name
+              }</p>
+              </div>
+          </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+// to consume fulfilled promise ==> .then() IMP
+// to read data from response ==> .json() ==> returns a NEW PROMISE IMP ==> call another .then()
+
+// we get a 'response' ==> turn it into 'json' ==> take the 'data' and render it into DOM
+const getCountryDataAndNeighbour = function (country) {
+  // Country 1
+  fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0]; // optional chaining
+
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(
+        `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
+      );
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
+};
+
+// instead of callback hell, we have a flat chain of promises IMP
+// ALWAYS return a promise and handle it outside, don't do this:
+
+/*
+fetch(`https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`).then()
+*/
+
+getCountryDataAndNeighbour('romania');
