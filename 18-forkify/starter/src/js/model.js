c35693e645e360af89dcf54fa311313b0054a9c4
Cloudiu9 <claudiubordea5@gmail.com>
2025-10-20 18:33:15 +0300

    Setting up config.js and helper.js, more refactoring

--- 18-forkify/starter/src/js/model.js
+++ 18-forkify/starter/src/js/model.js
@@ -1,3 +1,8 @@
+import { async } from 'regenerator-runtime';
+
+import { API_URL } from './config.js';
+import { getJSON } from './helpers.js';
+
 export const state = {
   recipe: {},
 };
@@ -5,12 +10,7 @@ export const state = {
 // Changes state object (not a pure function)
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.jonas.io/api/v2/recipes/${id}`
-    );
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    const data = await getJSON(`${API_URL}/${id}`);
 
     // console.log(res, data);
     const { recipe } = data.data; // destructuring since data.data.recipe exists
@@ -28,6 +28,6 @@ export const loadRecipe = async function (id) {
 
     console.log(state.recipe);
   } catch (err) {
-    alert(err);
+    console.error(`${err} 💥💥💥`);
   }
 };
