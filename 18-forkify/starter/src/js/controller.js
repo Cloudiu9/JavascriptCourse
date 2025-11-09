5d3a2d9c430baf48fd965d42fb24db2f267ca18c
Cloudiu9 <claudiubordea5@gmail.com>
2025-11-09 19:02:30 +0200

    Uploading a Recipe (1)

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -5,6 +5,7 @@ import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
 
 // polyfilling
 import 'core-js/stable';
@@ -97,6 +98,12 @@ const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
+const controlAddRecipe = function (newRecipe) {
+  console.log(newRecipe);
+
+  // Upload the new recipe data
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -104,5 +111,6 @@ const init = function () {
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults); // subscriber
   paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 init();
---
+++ 18-forkify/starter/src/js/views/addRecipeView.js
@@ -0,0 +1,48 @@
+import icons from 'url:../../img/icons.svg'; // parcel 2
+
+import View from './View';
+
+class AddRecipeView extends View {
+  _parentElement = document.querySelector('.upload');
+  _window = document.querySelector('.add-recipe-window');
+  _overlay = document.querySelector('.overlay');
+  _btnOpen = document.querySelector('.nav__btn--add-recipe');
+  _btnClose = document.querySelector('.btn--close-modal');
+
+  // using this because we don't need a controller to interfere with it
+  constructor() {
+    // to allow the use of 'this'
+    super();
+    this._addHandlerShowWindow();
+    this._addHandlerHideWindow();
+  }
+
+  toggleWindow() {
+    this._overlay.classList.toggle('hidden');
+    this._window.classList.toggle('hidden');
+  }
+
+  _addHandlerShowWindow() {
+    this._btnOpen.addEventListener('click', this.toggleWindow.bind(this)); // binding the 'this' keyword so it works correctly
+  }
+
+  _addHandlerHideWindow() {
+    this._btnClose.addEventListener('click', this.toggleWindow.bind(this)); // binding the 'this' keyword so it works correctly
+    this._overlay.addEventListener('click', this.toggleWindow.bind(this)); // binding the 'this' keyword so it works correctly
+  }
+
+  addHandlerUpload(handler) {
+    this._parentElement.addEventListener('submit', function (e) {
+      e.preventDefault();
+
+      // passing in the form as 'this' (because we're inside of a handler function of _parentElement)
+      const dataArr = [...new FormData(this)];
+      const data = Object.fromEntries(dataArr); // converts an array of entries to an object
+      handler(data);
+    });
+  }
+
+  _generateMarkup() {}
+}
+
+export default new AddRecipeView();
